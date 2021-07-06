ba75ec24f816245de3fa7503e88dc03fc9a79d92
damithc <damith@gmail.com>
2021-07-06 07:55:29 +0000

    Update gh-pages to output generated at 89b80ec

---
+++ dg/index.page-vue-render.js
@@ -0,0 +1,37 @@
+
+    var pageVueRenderFn = function anonymous(
+) {
+with(this){return _c('div',{attrs:{"id":"app"}},[_c('navbar',{attrs:{"type":"dark"},scopedSlots:_u([{key:"brand",fn:function(){return [_c('a',{staticClass:"navbar-brand",attrs:{"href":"/index.html","title":"Home"}},[_c('img',{attrs:{"src":"/images/catcher_logo.png","width":"40"}})])]},proxy:true},{key:"right",fn:function(){return [_c('li',[_c('form',{staticClass:"navbar-form"},[_c('searchbar',{attrs:{"data":searchData,"placeholder":"Search","on-hit":searchCallback,"menu-align-right":""}})],1)])]},proxy:true}])},[_v(" "),_c('li',[_c('a',{staticClass:"nav-link",attrs:{"href":"/index.html"}},[_v("HOME")])]),_v(" "),_c('li',[_c('a',{staticClass:"nav-link",attrs:{"href":"/ug/index.html"}},[_v(" USER GUIDE ")])]),_v(" "),_c('li',[_c('a',{staticClass:"nav-link",attrs:{"href":"/dg/index.html"}},[_v(" DEVELOPER GUIDE ")])]),_v(" "),_c('li',[_c('a',{staticClass:"nav-link",attrs:{"href":"/about-us.html"}},[_v(" ABOUT US ")])]),_v(" "),_c('li',[_c('a',{staticClass:"nav-link",attrs:{"href":"https://github.com/CATcher-org/CATcher","target":"_blank"}},[_c('span',[_c('span',{staticClass:"fab fa-github",attrs:{"aria-hidden":"true"}})])])])]),_v(" "),_c('div',{attrs:{"id":"flex-body"}},[_c('overlay-source',{staticClass:"fixed-header-padding",attrs:{"id":"site-nav","tag-name":"nav","to":"site-nav"}},[_c('div',{staticClass:"site-nav-top"},[_c('div',{staticClass:"font-weight-bold mb-2",staticStyle:{"font-size":"1.25rem"}},[_c('span',[_c('strong',[_c('strong',[_v("DEVELOPER GUIDE")])])])])]),_v(" "),_c('div',{staticClass:"nav-component slim-scroll"},[_c('div',[_c('site-nav',[_c('overlay-source',{staticClass:"site-nav-list site-nav-list-root",attrs:{"tag-name":"ul","to":"mb-site-nav"}},[_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/dg/index.html"}},[_v("DG Home")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/dg/user-workflow.html"}},[_v("User workflow")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/dg/tools.html"}},[_v("Tools")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/dg/setting-up.html"}},[_v("Setting up")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/dg/design.html"}},[_v("Design")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/dg/dev-workflow.html"}},[_v("Dev workflow")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/dg/testing.html"}},[_v("Testing")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/dg/project-management.html"}},[_v("Project management")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/dg/future-directions.html"}},[_v("Future directions")])])])])],1)],1)])]),_v(" "),_c('div',{staticClass:"fixed-header-padding",attrs:{"id":"content-wrapper"}},[_m(0),_v(" "),_c('p',[_v("Welcome to the CATcher Developer Guide! Given below is an overview of various sections of CATcher dev docs.")]),_v(" "),_c('box',{attrs:{"type":"tip","seamless":""}},[_c('p',[_v("If you are a new developer, you'll find it useful to read the first few sections below in the given order.")])]),_v(" "),_c('ol',[_c('li',[_c('p',[_m(1),_v(" explains how CATcher is used by various user-roles involved, at different phases of a CATcher-assisted "),_c('span',{directives:[{name:"b-tooltip",rawName:"v-b-tooltip.hover.top.html",value:(tooltipInnerContentGetter),expression:"tooltipInnerContentGetter",modifiers:{"hover":true,"top":true,"html":true}}],staticClass:"trigger",attrs:{"data-mb-component-type":"tooltip"}},[_c('span',{attrs:{"data-mb-slot-name":"_content"}},[_v("Practical Exam")]),_v("PE")]),_v(".")])]),_v(" "),_m(2),_v(" "),_m(3),_v(" "),_m(4),_v(" "),_m(5),_v(" "),_m(6),_v(" "),_m(7),_v(" "),_m(8)]),_v(" "),_c('i',{staticClass:"fa fa-arrow-circle-up fa-lg d-print-none",attrs:{"id":"scroll-top-button","onclick":"handleScrollTop()","aria-hidden":"true"}})],1)],1),_v(" "),_m(9)],1)}
+};
+    var pageVueStaticRenderFns = [function anonymous(
+) {
+with(this){return _c('h1',{attrs:{"id":"catcher-developer-guide"}},[_c('span',{staticClass:"anchor",attrs:{"id":"catcher-developer-guide"}}),_v("CATcher Developer Guide"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#catcher-developer-guide","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('strong',[_v("The "),_c('a',{attrs:{"href":"/dg/user-workflow.html"}},[_c('em',[_v("User workflow")])]),_v(" page")])}
+},function anonymous(
+) {
+with(this){return _c('li',[_c('p',[_c('strong',[_v("The "),_c('a',{attrs:{"href":"/dg/tools.html"}},[_c('em',[_v("Tools")])]),_v(" page")]),_v(" introduces the tools used by this project.")])])}
+},function anonymous(
+) {
+with(this){return _c('li',[_c('p',[_c('strong',[_v("The "),_c('a',{attrs:{"href":"/dg/setting-up.html"}},[_c('em',[_v("Setting up")])]),_v(" page")]),_v(" explains how to set up the dev environment in your computer.")])])}
+},function anonymous(
+) {
+with(this){return _c('li',[_c('p',[_c('strong',[_v("The "),_c('a',{attrs:{"href":"/dg/design.html"}},[_c('em',[_v("Design")])]),_v(" page")]),_v(" explains the high-level design of CATCher.")])])}
+},function anonymous(
+) {
+with(this){return _c('li',[_c('p',[_c('strong',[_v("The "),_c('a',{attrs:{"href":"/dg/dev-workflow.html"}},[_c('em',[_v("Dev workflow")])]),_v(" page")]),_v(" explains the workflow to be followed by devs.")])])}
+},function anonymous(
+) {
+with(this){return _c('li',[_c('p',[_c('strong',[_v("The "),_c('a',{attrs:{"href":"/dg/testing.html"}},[_c('em',[_v("Testing")])]),_v(" page")]),_v(" explains how CATcher is being tested.")])])}
+},function anonymous(
+) {
+with(this){return _c('li',[_c('p',[_c('strong',[_v("The "),_c('a',{attrs:{"href":"/dg/project-management.html"}},[_c('em',[_v("Project management")])]),_v(" page")]),_v(" has information on project management activities to be done by senior members of the dev team.")])])}
+},function anonymous(
+) {
+with(this){return _c('li',[_c('p',[_c('strong',[_v("The "),_c('a',{attrs:{"href":"/dg/future-directions.html"}},[_c('em',[_v("Future directions")])]),_v(" page")]),_v(" lists some ideas for improving CATcher in the future.")])])}
+},function anonymous(
+) {
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 3.0.4")]),_v(", generated on Tue, 6 Jul 2021, 7:55:27 UTC]")])])])}
+}];
+  
