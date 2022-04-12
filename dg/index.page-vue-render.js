950136647900cca4ddd002829f08df5ad9f8d4d9
seanlowjk <42912708+seanlowjk@users.noreply.github.com>
2022-04-12 09:55:55 +0000

    Update gh-pages to output generated at 833a1c5

--- dg/index.page-vue-render.js
+++ dg/index.page-vue-render.js
@@ -32,6 +32,6 @@ with(this){return _c('li',[_c('p',[_c('strong',[_v("The "),_c('a',{attrs:{"href"
 with(this){return _c('li',[_c('p',[_c('strong',[_v("The "),_c('a',{attrs:{"href":"/dg/future-directions.html"}},[_c('em',[_v("Future directions")])]),_v(" page")]),_v(" lists some ideas for improving CATcher in the future.")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 3.1.1")]),_v(", generated on Mon, 28 Mar 2022, 4:24:07 UTC]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 3.1.1")]),_v(", generated on Tue, 12 Apr 2022, 9:55:53 UTC]")])])])}
 }];
   
